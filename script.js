3b6e4738b71275a42ae54aad02f49d7735a74aae
Olivia Wilson-Simmonds <heavenwilson210@gmail.com>
2025-06-18 14:00:38 -0400

    Final Version

--- script.js
+++ script.js
@@ -1,54 +1,50 @@
 console.log("Hello! If you see this, the script is working.");
 
-/*
-- [ ] Select the section with an id of container without using querySelector.
 
+/*- [ ] Select the section with an id of container without using querySelector.*/
+let selectContainer = document.getElementById("container");
 
+console.log(selectContainer);
 
+/*- [ ] Select the section with an id of container using querySelector.*/
+let querySelectContainer = document.querySelector("#container");
 
+console.log(querySelectContainer);
 
-- [ ] Select the section with an id of container using querySelector.
-
-
-
-
-- [ ] Select all of the list items with a class of "second".*/
-const seconds = document.querySelectorAll(".second");
+/*- [ ] Select all of the list items with a class of "second".*/
+let seconds = document.querySelectorAll(".second");
 
 seconds.forEach((item, index) => {
   console.log(`.second item ${index + 1}:`, item);
 });
 
+/*- [ ] Select a list item with a class of third, but only the list item inside of the ol tag.*/
+const thirdItem = document.querySelector('ol li.third');
+console.log("List item with a class of third inside of the ol tag: ", thirdItem.textContent);
+
+
+/*- [ ] Give the section with an id of container the text "Hello!".*/
+const container = document.getElementById("container").innerHTML = "Hello!";
+console.log("Give the section with an id of container the text 'Hello!': ", container);
+
+
+/*- [ ] Add the class main to the div with a class of footer.*/
+const divFooter = document.getElementsByClassName("footer")[0];
+divFooter.classList.add('main');
+console.log("Add the class main to the div with a class of footer", divFooter);
+
+/*- [ ] Remove the class main on the div with a class of footer.*/
+const footer = document.querySelector('.footer');
+footer.classList.remove("main");
+
+
+/*- [ ] Create a new li element.*/
+const newLi = document.createElement("li");
 
 
 
-/*- [ ] Select a list item with a class of third, but only the list item inside of the ol tag.
-
-
-
-
-- [ ] Give the section with an id of container the text "Hello!".
-
-
-
-
-- [ ] Add the class main to the div with a class of footer.
-
-
-
-
-- [ ] Remove the class main on the div with a class of footer.
-
-
-
-
-- [ ] Create a new li element.
-
-
-
-
-- [ ] Give the li the text "four".*/
-
+/*- [ ] Give the li the text "four".*/
+newLi.textContent="four";
 
 
 
@@ -63,8 +59,8 @@ olGrn.forEach(li => {
   li.style.backgroundColor = "green";
 });
 /*- [ ] Remove the div with a class of footer.*/
-footerDiv.remove();
-
+divFooter.remove();
+console.log("Removed footer div.");
 
 
 // Try rewriting this without using querySelector
